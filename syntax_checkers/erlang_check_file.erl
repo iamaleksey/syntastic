cf689fcd1eeb000d6f2b175a106e181505f40245
Aleksey Yeschenko <aleksey@yeschenko.com>
2012-01-15 00:09:36 +0300

    Add rebar's deps_dir to the list of include paths so that include_lib works

--- syntax_checkers/erlang_check_file.erl
+++ syntax_checkers/erlang_check_file.erl
@@ -2,11 +2,19 @@
 -export([main/1]).
 
 main([FileName]) ->
-    compile:file(FileName, [warn_obsolete_guard,
-                            warn_unused_import,
-                            warn_shadow_vars,
-                            warn_export_vars,
-                            strong_validation,
-                            report,
-                            {i, filename:dirname(FileName) ++ "/../include"}
-                        ]).
+    BaseDir = filename:join(filename:dirname(FileName), ".."),
+    DepsDir = case file:consult(filename:join(BaseDir, "rebar.config")) of
+                  {ok, Terms} ->
+                      proplists:get_value(deps_dir, Terms, "deps");
+                  {error, _} ->
+                      "deps"
+              end,
+    compile:file(FileName,
+                 [warn_obsolete_guard,
+                  warn_unused_import,
+                  warn_shadow_vars,
+                  warn_export_vars,
+                  strong_validation,
+                  report,
+                  {i, filename:join(BaseDir, "include")},
+                  {i, filename:join(BaseDir, DepsDir)}]).
